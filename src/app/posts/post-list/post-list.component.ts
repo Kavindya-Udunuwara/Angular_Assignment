c3b7cd7cdba5ad76de81c38dfb13ce663b43d5ed
Kavindya-Udunuwara <udunuwaraukc@gmail.com>
2025-05-17 23:18:35 +0530

    post list

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { Post } from '../../models/post.model';
+import { CommonModule } from '@angular/common';
+import { PostCardComponent } from '../post-card/post-card.component'; 
+
+
+@Component({
+  selector: 'app-post-list',
+  standalone: true,
+  imports: [CommonModule, PostCardComponent ],
+  templateUrl: './post-list.component.html',
+  styleUrl: './post-list.component.scss'
+})
+
+export class PostListComponent implements OnInit {
+  posts: Post[] = [];
+  currentPage: number = 0;
+  hasMore: boolean = true;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.loadMore();
+  }
+
+  loadMore(): void {
+    const response = this.postService.getPosts(this.currentPage);
+    this.posts.push(...response.result);
+    this.hasMore = response.next;
+    this.currentPage++;
+  }
+
+  truncate(text: string, maxLength: number = 100): string {
+    return text.length > maxLength ? text.slice(0, maxLength) + '...' : text;
+  }
+}
