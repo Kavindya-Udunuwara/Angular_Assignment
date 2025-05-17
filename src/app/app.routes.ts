9859085dc2296d78cfd8a125c4de60ea95cf9d74
Kavindya-Udunuwara <udunuwaraukc@gmail.com>
2025-05-17 23:20:07 +0530

    app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostDetailComponent } from './posts/post-detail/post-detail.component';
+
+export const routes: Routes = [
+  { path: '', component: PostListComponent },
+  { path: 'posts/:id', component: PostDetailComponent },
+  { path: '**', redirectTo: '' },
+];
