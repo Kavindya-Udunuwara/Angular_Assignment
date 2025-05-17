b2fcea54dea3e1a41703a4a25fe311edc483567c
Kavindya-Udunuwara <udunuwaraukc@gmail.com>
2025-05-18 00:01:11 +0530

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { CommonModule } from '@angular/common';
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: PostListComponent },
-      { path: 'posts/:id', component: PostDetailsComponent },
+      { path: 'posts/:id', component: PostDetailComponent },
       { path: '**', redirectTo: '' }
     ])
   ],
