80a5bb3de971106291b5445a14ed87b29a77826d
Kavindya-Udunuwara <udunuwaraukc@gmail.com>
2025-05-17 23:19:20 +0530

    post details

---
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { PostService } from '../../services/post.service';
+import { Post, Comment } from '../../models/post.model';
+import { CommonModule } from '@angular/common';
+import { CommentComponent } from '../comment/comment.component';
+
+@Component({
+  selector: 'app-post-detail',
+  standalone: true,
+  imports: [CommonModule, CommentComponent],
+  templateUrl: './post-detail.component.html',
+  styleUrls: ['./post-detail.component.scss'], // fixed from styleUrl
+})
+export class PostDetailComponent implements OnInit {
+  post!: Post;
+  comments: Comment[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    this.post = this.postService.getPost(id);
+    this.comments = this.postService.getComments(id);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
